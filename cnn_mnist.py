a791d7b1b2eb7054940e9046e846169b7c450bc1
EphesosX <tkt5@njit.edu>
2018-02-28 16:07:22 -0800

    old attempt to get weights

--- cnn_mnist.py
+++ cnn_mnist.py
@@ -33,16 +33,28 @@ def cnn_model_fn(features, labels, mode):
     pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2,2],strides=2)
     
     pool2_flat = tf.reshape(pool2, [-1,7*7*64])
-    dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+    Dense = tf.layers.Dense(units=1024, activation=tf.nn.relu)
+    dense = Dense.apply(pool2_flat)
     dropout = tf.layers.dropout(
             inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
     
     logits = tf.layers.dense(inputs=dropout, units=10)
     
+    weights1 = Dense.apply(tf.convert_to_tensor(np.eye(7*7*64, dtype='float32')))
+    
+
+#    weights = tf.get_default_graph().get_tensor_by_name('dense_1/kernel:0')
+    weights = tf.get_default_graph().get_tensor_by_name('dense/Relu_1:0')
+    bias = tf.get_default_graph().get_tensor_by_name('dense/bias:0')
+    conv_kernel = tf.get_default_graph().get_tensor_by_name('conv2d/kernel:0')
+
     predictions = {
             "classes": tf.arg_max(input=logits, dimension=1),
             "probabilities": tf.nn.softmax(logits, name="softmax_tensor"),
             "logits": logits,
+            "conv_kernel": conv_kernel,
+            "dense_kernel": weights,
+            "dense_bias": bias,
             "conv1": conv1,
             "conv2": conv2}
     # "smuggle" out logits
@@ -71,11 +83,6 @@ def cnn_model_fn(features, labels, mode):
     return tf.estimator.EstimatorSpec(
             mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
-def mnist_direct_data_input_fn(features_np_dict, targets_np):
-    features_dict = { k:tf.constant(v) for k,v in features_np_dict.items()}
-    targets = None if targets_np is None else tf.constant(targets_np)
-    return features_dict, targets
-
 def main(unused_argv):
     mnist = tf.contrib.learn.datasets.load_dataset("mnist")
     train_data = mnist.train.images
@@ -87,36 +94,46 @@ def main(unused_argv):
     tensors_to_log = {"probabilities": "softmax_tensor"}
     logging_hook = tf.train.LoggingTensorHook(
             tensors=tensors_to_log, every_n_iter=50)
-    train_input_fn = tf.estimator.inputs.numpy_input_fn(
-            x={"x": train_data},
-               y=train_labels,
-               batch_size=100,
-               num_epochs=None,
-               shuffle=True)
-    mnist_classifier.train(
-            input_fn=train_input_fn,
-            steps=1,
-            hooks=[logging_hook])
+#    train_input_fn = tf.estimator.inputs.numpy_input_fn(
+#            x={"x": train_data},
+#               y=train_labels,
+#               batch_size=100,
+#               num_epochs=None,
+#               shuffle=True)
+#    mnist_classifier.train(
+#            input_fn=train_input_fn,
+#            steps=1,
+#            hooks=[logging_hook])
 
+    print(type(eval_data))
+    print(type(eval_labels))
 
     tensor_prediction_generator = mnist_classifier.predict( 
         input_fn=tf.estimator.inputs.numpy_input_fn(
-            x={"x": eval_data},
-            y=eval_labels,
+            x={"x": eval_data[0:1]},
+            y=eval_labels[0:1],
             num_epochs=1,
             shuffle=False),
-        predict_keys=['logits', 'conv1', 'conv2']
+        predict_keys=['dense_kernel']
     )
     
-    for tensor_predictions in tensor_prediction_generator:
-        break # Get the first one...
     
-    print("########### LOGITS ###########")
-    print(tensor_predictions['logits'])
-    print("########### CONV1 ###########")
-    print(tensor_predictions['conv1'])
-    print("########### CONV2 ###########")
-    print(tensor_predictions['conv2'])
+#    dense_weights = [tensor_predictions['dense_kernel'] for tensor_predictions in tensor_prediction_generator]
+    
+#    print(len([1 for x in tensor_prediction_generator]))
+
+    for tensor_predictions in tensor_prediction_generator:
+#        print(tensor_predictions['conv_kernel'])
+        print(tensor_predictions['dense_kernel'])
+#        print(tensor_predictions['logits'])
+        break
+    
+#    print("########### LOGITS ###########")
+#    print(tensor_predictions['logits'])
+#    print("########### CONV1 ###########")
+#    print(tensor_predictions['conv1'])
+#    print("########### CONV2 ###########")
+#    print(tensor_predictions['conv2'])
 #    eval_input_fn = tf.estimator.inputs.numpy_input_fn(
 #            x={"x": eval_data},
 #            y=eval_labels,
@@ -124,6 +141,12 @@ def main(unused_argv):
 #            shuffle=False)
 #    eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
 #    print(eval_results)
+    print([v.name for v in tf.all_variables()])
+
+#    sess = tf.Session()
+#    init = tf.global_variables_initializer()
+#    sess.run(init)
+#    print(sess.run([v for v in tf.all_variables()]))
     
 if __name__ == "__main__":
     tf.app.run()
